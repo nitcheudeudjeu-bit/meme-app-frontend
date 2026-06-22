ab8f1d2e4010ac687618c80087186ee38e3f8a85
Indirha-Dansi <indirhadansi@gmail.com>
2026-06-22 09:13:21 +0100

    modifier le nom de src

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 // On ajoute l'extension .tsx explicite pour contourner le bug de localisation
-import AppNavigator from './scr/AppNavigator';
+import AppNavigator from './src/AppNavigator';
 
 function App(): React.JSX.Element {
   return (
