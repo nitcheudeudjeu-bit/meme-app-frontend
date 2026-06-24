543cfba577a8f0e10ca04898da029518a7206d8b
Indirha-Dansi <indirhadansi@gmail.com>
2026-06-24 15:27:41 +0100

    les 3 boutons avec la partie de Miasse et modules d'images

--- App.tsx
+++ App.tsx
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+
 import AppNavigator from './src/AppNavigator.tsx';
 import SplashScreen from './src/screens/SplashScreen.tsx';
 
 function App(): React.JSX.Element {
-  // Variable d'état pour savoir si l'animation de démarrage est finie
   const [isSplashFinished, setIsSplashFinished] = useState<boolean>(false);
 
   return (
     <SafeAreaProvider style={styles.container}>
-      {isSplashFinished ? (
-        // Une fois fini, on affiche le Hub et les Onglets de l'application
-        <AppNavigator />
-      ) : (
-        // Au tout premier clic sur l'icône, on affiche la page d'attente animée
-        <SplashScreen onFinish={() => setIsSplashFinished(true)} />
-      )}
+      <StatusBar barStyle="light-content" backgroundColor="#0d0d14" />
+      
+      {/* 🚀 SOLUTION : Le conteneur enveloppe TOUTE l'application dès le départ */}
+      <NavigationContainer>
+        {isSplashFinished ? (
+          <AppNavigator />
+        ) : (
+          <SplashScreen onFinish={() => setIsSplashFinished(true)} />
+        )}
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
@@ -24,7 +28,7 @@ function App(): React.JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0d0d14', // Synchronisé avec la couleur de fond de votre Splash
+    backgroundColor: '#0d0d14',
   },
 });
 
